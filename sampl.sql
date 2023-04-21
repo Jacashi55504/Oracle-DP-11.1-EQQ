040d40085a83a3a84c5ae81ae8a87faf5d1a71a1
Jav CS <127455737+Jacashi55504@users.noreply.github.com>
2023-04-20 23:35:54 -0600

    Update sampl.sql

--- sampl.sql
+++ sampl.sql
@@ -5,7 +5,7 @@ SELECT table_name
 FROM user_tables
 WHERE table_name LIKE 'JO%';
 
--- 2. – Create a list that includes the first initial of every employee's first name, a space, and the last name of the employee.
+-- 2. Create a list that includes the first initial of every employee's first name, a space, and the last name of the employee.
 
 SELECT SUBSTR(first_name, 1, 1) || ' ' || last_name AS "Employee Names"
 FROM employees;
