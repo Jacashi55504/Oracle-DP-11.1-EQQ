7d0b0c3dbf892fa61530ad24442af76f840a9df2
Jav CS <127455737+Jacashi55504@users.noreply.github.com>
2023-04-21 00:41:33 -0600

    Update sampl.sql

--- sampl.sql
+++ sampl.sql
@@ -108,7 +108,7 @@ SELECT LEVEL AS POSITION, e.last_name, m.last_name AS MANAGER_NAME
 FROM employees e LEFT OUTER JOIN employees m
  ON (e.manager_id = m.employee_id)
 START WITH e.employee_id = 100 -- (e.employee_id IS NULL) BETTER OPTIMIZATION
-CONNECTBY PRIOR e.employee_id = e.manager_id
+CONNECTBY PRIOR e.employee_id = e.manager_id;
 
 -- 17. Produce a list of the earliest hire date, the latest hire date, and the number of employees from the employees table
 SELECT MIN(hire_date) AS "Lowest", MAX(hire_date) AS "Highest", COUNT(employee_id) AS "No of employees"
@@ -121,7 +121,7 @@ FROM Employees e LEFT OUTER JOIN departments d
  ON (d.department_id = e.department_id)
 GROUP BY d.department_name
 HAVING SUM(e.salary) BETWEEN 15000 AND 31000
-ORDER BY d.department_name
+ORDER BY d.department_name;
 
 
 -- 19. Show the highest average salary for the departments in the employees table.
